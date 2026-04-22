435bd3429c1fa7d130d5403204fa7762023c3a18
Arjun Divraniya <arjun.divraniya.cg@gmail.com>
2026-04-22 15:31:21 +0530

    Add additional output statements to Main class

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("We Are Test jenkins");
+        System.out.println("From VSCode");
+        System.out.println("From GitHub");
     }
 }
