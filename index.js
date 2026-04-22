db143b8fe0de55ac9bba3823a2cbbdbdecf94d85
Arjun Divraniya <arjun.divraniya.cg@gmail.com>
2026-04-22 15:47:13 +0530

    Add initial output statements to index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("Hello From JavaScript");
+console.log("We Are Test jenkins");
+console.log("From VSCode");
+console.log("From GitHub");
+console.log("From Jenkins");
+console.log("From Local Machine");
