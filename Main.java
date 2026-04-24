060f6ffb9e81164b52ba9caab9ddc3101c17269a
Ubuntu <ubuntu@ip-172-31-12-251.ap-south-1.compute.internal>
2026-04-24 08:41:58 +0000

    Add from AWS

--- Main.java
+++ Main.java
@@ -5,6 +5,6 @@ public class Main {
         System.out.println("From VSCode");
         System.out.println("From GitHub");
         System.out.println("From Jenkins");
-        System.out.println("From Local Machine");
+        System.out.println("From AWS Ubuntu Machine");
     }
 }
