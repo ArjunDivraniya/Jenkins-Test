848710c0a2f79f609c4b9fab601248ebd40e64ca
Ubuntu <ubuntu@ip-172-31-12-251.ap-south-1.compute.internal>
2026-04-24 09:09:47 +0000

    Second time

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ console.log("We Are Test jenkins");
 console.log("From VSCode");
 console.log("From GitHub");
 console.log("From Jenkins");
-console.log("From Local Machine");
+console.log("From AWS Ubuntu Machine");
 console.log("Commit on Arjun Branch -> Main");
